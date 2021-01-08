55eb91afe763166cb30d7659acad741217defc58
Williams <wilfre_padilla@hotmail.com>
2021-01-08 08:43:17 -0400

    Feat: adding modified data in the update and delete todo endpoint

--- api/models/Todo.js
+++ api/models/Todo.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const Todo = mongoose.model('Todo', new Schema({
     title: String,
     desc: String,
+    user_id: { type: Schema.Types.ObjectId, ref: 'User' },
+
 }));
 
 module.exports = Todo;
--- api/routes/todos.js
+++ api/routes/todos.js
@@ -6,23 +6,25 @@ const router = express.Router();
 const Todos = require('../models/Todo');
 
 router.get('/', isAuthenticated, (req, res) => {
-    Todos.find().exec().then(x => res.status(200).send(x));
+    const { _id } = req.user;
+    Todos.find({user_id: _id}).exec().then(x => res.status(200).send({ data: x }));
 });
 
 
 router.get('/:id', isAuthenticated, (req, res) => {
-    Todos.findById(req.params.id).exec().then(x => res.status(200).send(x));
+    Todos.findById(req.params.id).exec().then(x => res.status(200).send({ data: x }));
 });
 
 router.post('/', isAuthenticated, (req, res) => {
-    Todos.create(req.body).then(x => res.status(201).send(x));
+    const { _id } = req.user;
+    Todos.create({...req.body, user_id: _id }).then( x => res.status(201).send({ data: x }));
 });
 
 router.put('/:id', isAuthenticated, (req, res) => {
-    Todos.findOneAndUpdate(req.params.id, req.body).then(x => res.status(204).send('Success!'));
+    Todos.findOneAndUpdate(req.params.id, req.body).then(x => res.status(200).send({message: 'Success!', data: x }));
 });
 
 router.delete('/:id', isAuthenticated, (req, res) => {
-    Todos.findOneAndDelete(req.params.id, req.body).then(x => res.status(204).send('Success!'));
+    Todos.findOneAndDelete(req.params.id, req.body).then(x => res.status(200).send({message: 'Success!', data: x}));
 });
 module.exports = router;
